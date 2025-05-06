99e1c319721224af3b767a334822480c13304b32
deebesh05052004 <deebesh2004@gmail.com>
2025-05-06 13:18:40 +0530

    Create delivery price calculation.py

---
+++ delivery price calculation.py
@@ -0,0 +1,9 @@
+weight = float(input("Enter the weight in kg : "))
+distance = float(input("Enter the distance in KM : "))
+base_price = 50
+weight_price = weight*10
+distance_price = distance*5
+final_amount = base_price + distance_price + weight_price
+print("Final amount to be paid : " ,final_amount )
+print("THANKS FOR CHOOSING OUR SERVICE")
+print("HAVE A NICE DAY")
